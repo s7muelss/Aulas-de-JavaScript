1d2e4ed31d99e2261737f888f0b1a37b158f6365
samuel <samuel.sousauni@gmail.com>
2026-02-24 22:12:01 -0300

    Aula 1 - Variaveis JavaScript

---
+++ Aula-1/script.js
@@ -0,0 +1,19 @@
+console.log("Testando JavaScript");
+// Exemplo de variáveis //
+var nome = "Samuel";
+
+var idade = 19;
+
+var cidade = "São Paulo";
+
+console.log(nome);
+
+console.log(idade);
+
+console.log(cidade);
+
+// Exemplo de constantes //
+const times = ["são paulo", "corinthians", "santos", "palmeiras"];
+
+console.log(times);
+
