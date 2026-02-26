0bbda270518dab4ce0551a7f4b4a8e3892ff41ac
samuel <samuel.sousauni@gmail.com>
2026-02-25 22:16:20 -0300

    Aula-2 Operadores

---
+++ Aula-2/script.js
@@ -0,0 +1,96 @@
+// Exemplos de Atribuicao //
+let x = 10;
+
+x = x + 5;
+//console.log(x);
+
+x = x + 5;
+//console.log(x);
+
+// Exemplos de aritmetrica em JavaScript //
+let soma = 5 + 5 + 4;
+console.log(soma);
+
+// const primeiroNome = "Samuel";
+// const sobrenome = "Sousa";
+
+let nomeCompleto = "Samuel" + " " + "Sousa";
+
+console.log(nomeCompleto);
+
+// console.log (nomeCompleto);
+
+// Exemplos de tipos de JavaScript //
+// String
+
+let cor = "Azul";
+let fruta = "Maçã";
+
+console.log(cor);
+console.log(fruta);
+
+// Number
+let soma1 = 10 + 5;
+let soma2 = 20 - 10;
+console.log(soma1);
+console.log(soma2);
+
+// BigInt
+let bigInt1 = 1234567890123456789012345678901234567890n;
+let bigInt2 = 9876543210987654321098765432109876543210n;
+console.log(bigInt1);
+console.log(bigInt2);
+
+// Boolean
+let valorVerdadeiro = true;
+let valorFalso = false;
+console.log(valorVerdadeiro);
+console.log(valorFalso);
+
+// object
+const smartphone = {
+    marca: 'Apple',
+    modelo: 'iPhone 13',
+    ano: 2021
+};
+
+console.log(smartphone);
+
+// array
+const frutas = ['Maçã', 'Banana', 'Laranja', 'Uva'];
+console.log(frutas);
+
+// Date Object
+const data = new Date.now;
+
+console.log(data);
+
+// Undefined
+let indefinido;
+
+console.log(indefinido);
+
+// Null
+let nulo = null;
+
+console.log(nulo);
+
+// Symbol
+let y = symbol();
+
+console.log(y);
+
+// Exemplo de typeof
+let stringTipo = "300";
+let numberTipo = 300;
+
+// const tipoDado = typoff stringTipo;
+const tipoDado = typeof numberTipo;
+
+console.log(tipoDado);
+
+// Exemplo de Boolean
+let cinco = 5;
+let comparacao = cinco === 8;
+
+console.log(comparacao);
---
+++ Aula-2/script02.js
@@ -0,0 +1,27 @@
+// Exemplos de operacoes aritmeticas em JavaScript //
+let a = 5;
+let x = (100 + 200) * a;
+
+console.log(x);
+
+// Exemplo de adicao
+let texto1= "somente a dor coletiva";
+
+texto1 = texto1 + " gera uniao";
+
+// texto1 += " gera uniao";
+
+console.log(texto1);
+
+// Exemplo de incremento
+let w = 5;
+w++;
+let z = w + 10;
+console.log(z);
+
+// Exemplo de decremento
+letb = 5;
+b--;
+let c = b + 10;
+console.log(c);
+
