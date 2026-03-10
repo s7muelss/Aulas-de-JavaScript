6607b017d7b66d2d145c48e1b52edefc2f43a2e4
samuel <samuel.sousauni@gmail.com>
2026-03-09 22:09:29 -0300

    Aula-9

---
+++ Aula-9/script.js
@@ -0,0 +1,30 @@
+// Utilizacao do toString()
+let numero = 123;
+let exToString = numero.toString();
+console.log(typeof exToString);
+
+//Utilizacao do toExponential()
+let a = 9.646;
+let exToExponential = a.toExponential(2);
+console.log(exToExponentialExponential);
+
+// Utilizacao do toFixed()
+let b = 44.23456;
+let exToFixed = b.toFixed(2);
+console.log(exToFixed);
+
+//Utilizacao do toPrecision()
+let c = 63.875436;
+let exToPrecision = c.toPrecision(3);
+console.log(exToPrecision);
+
+// Utilizacao do valueOf()
+let d = 1993;
+let exValueOf = d.valueOf();
+console.log(exValueOf);
+
+//Utilizacao do Number();
+let e = "593";
+let exNumber = Number (e);
+console.log(exNumber);
+
---
+++ Aula-9/script02.js
@@ -0,0 +1,24 @@
+//Utilizacao da propriedade EPSILON
+let a = Number.EPSILON;
+console.log(a);
+
+//Utilizacao da propriedade MAX_VALUE
+let b = Number.MAX_VALUE;
+console.log(b);
+
+//Utilizacao da propriedade MIN_VALUE
+let c = Number.MIN_VALUE;
+console.log
+
+//Utilizacao da propriedade MIN_SAFE_INTEGER
+let d = Number.MIN_SAFE_INTEGER;
+console.log(d);
+
+//Utilizacao da propriedade MAX_SAFE_INTEGER
+let e = Number.MAX_SAFE_INTEGER;
+console.log(e);
+
+//Utilizacao da propriedade POSITIVE_INFINITY
+let f = Number.POSITIVE_INFINITY;
+console.log(f);
+
